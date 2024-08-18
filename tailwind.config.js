43e3f487f0cf248b6d291367b58e9192861216b7
AMS <111883236+AMS003010@users.noreply.github.com>
2024-08-19 00:11:28 +0530

    feat: added final tailwind build support

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'src/assets/templates/document.html',
+    'src/assets/templates/page.html',
+    'src/assets/templates/column.html',
+    'src/assets/templates/calender.html',
+    'src/assets/templates/clock.html',
+    'src/assets/templates/weather.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
