ff7e90ee98d98077065fd74bb81e6269dd59358f
AMS <111883236+AMS003010@users.noreply.github.com>
2024-12-01 23:00:20 +0530

    chore: jus some emojis

--- src/internals/render.rs
+++ src/internals/render.rs
@@ -146,7 +146,7 @@ pub async fn final_yaml_to_html_render(data_config: &Data<Config>, mut final_htm
                 }
             }
             let duration = start.elapsed();
-            println!("🔵 Rendered in {:?}", duration);
+            println!("♾️  Rendered in {:?}", duration);
             final_html
         }
         Err(e) => {
