f9304a75d5e09ff889ae40ce63ec900a2aac3d69
AMS <111883236+AMS003010@users.noreply.github.com>
2024-12-18 13:47:21 +0530

    chore: made some chnages in the timezone offsets

--- src/feed/clock_data.rs
+++ src/feed/clock_data.rs
@@ -4,10 +4,10 @@ use std::collections::HashMap;
 fn get_timezone_for_place(place: &str) -> Option<chrono::FixedOffset> {
     let mut place_to_timezone = HashMap::new();
     place_to_timezone.insert("New York", chrono::FixedOffset::west_opt(4 * 3600));
-    place_to_timezone.insert("London", chrono::FixedOffset::east_opt(1 * 3600));
+    place_to_timezone.insert("London", chrono::FixedOffset::east_opt(0 * 3600));
     place_to_timezone.insert("Tokyo", chrono::FixedOffset::east_opt(9 * 3600));
     place_to_timezone.insert("Delhi", chrono::FixedOffset::east_opt(5 * 3600 + 1800));
-    place_to_timezone.insert("Los Angeles", chrono::FixedOffset::west_opt(7 * 3600));
+    place_to_timezone.insert("Los Angeles", chrono::FixedOffset::west_opt(8 * 3600));
     // println!("---> clock_data.rs // get_timezone_for_place");
     place_to_timezone.get(place).and_then(|opt| *opt)
 }
